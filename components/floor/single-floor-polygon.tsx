b7e30f188ef4acf3bc0cbc43583fe5920d79db19
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-05-30 21:08:04 +0400

    added red text if polygon is sold

--- components/floor/single-floor-polygon.tsx
+++ components/floor/single-floor-polygon.tsx
@@ -9,30 +9,52 @@ type Props = {
   flat: Flat;
 };
 
+const textPoints = [
+  { x: 100, y: 500 },
+  { x: 240, y: 320 },
+  { x: 440, y: 320 },
+  { x: 700, y: 320 },
+  { x: 1000, y: 320 },
+  { x: 440, y: 580 },
+  { x: 690, y: 580 },
+];
+
 const SingleFloorPolygon = ({
   hoveredPolygon,
   handlePolygonHover,
   handlePolygonLeave,
   flat,
 }: Props) => {
-  const { status } = useSession()
-  const { push } = useRouter()
+  const { status } = useSession();
+  const { push } = useRouter();
 
   const handlePageChange = () => {
-      if(status === 'unauthenticated' && flat.condition !== Condition.sold){
-        push({
-          pathname: `/residence/floor/${flat.floor}`,
-          query: {flat: flat.flatNum}
-        })
-      } else if(status === 'authenticated'){
-        push({
-          pathname: `/residence/floor/update/${flat.floor}`,
-          query: {flat: flat.flatNum}
-        })
-      }
-  }
+    if (status === "unauthenticated" && flat.condition !== Condition.sold) {
+      push({
+        pathname: `/residence/floor/${flat.floor}`,
+        query: { flat: flat.flatNum },
+      });
+    } else if (status === "authenticated") {
+      push({
+        pathname: `/residence/floor/update/${flat.floor}`,
+        query: { flat: flat.flatNum },
+      });
+    }
+  };
+
+  const textPoints = [
+    { x: 100, y: 500, flats: [1, 9, 17, 25, 33] },
+    { x: 240, y: 320, flats: [2, 10, 18, 26, 34] },
+    { x: 440, y: 320, flats: [3, 11, 19, 27, 35] },
+    { x: 700, y: 320, flats: [4, 12, 20, 28, 36] },
+    { x: 1000, y: 320, flats: [5, 13, 21, 29, 37] },
+    { x: 1040, y: 580, flats: [6, 14, 22, 30, 38] },
+    { x: 440, y: 600, flats: [7, 15, 23, 31, 39] },
+    { x: 690, y: 600, flats: [8, 16, 24, 32, 40] },
+  ];
 
   return (
+    <g>
       <polygon
         points={flat.points}
         className={`${
@@ -50,6 +72,21 @@ const SingleFloorPolygon = ({
         onMouseLeave={handlePolygonLeave}
         onClick={handlePageChange}
       />
+      {flat.condition === Condition.sold &&
+        textPoints.map((textPoint, idx) => {
+          if (textPoint.flats.includes(flat.flatNum)) {
+            return (
+              <text
+                x={textPoint.x}
+                y={textPoint.y}
+                className="text-4xl font-bold fill-red-500"
+              >
+                SOLD
+              </text>
+            );
+          }
+        })}
+    </g>
   );
 };
 
