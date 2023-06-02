36c712371b8b232f324e2a3d10626fe6d7922973
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-06-02 21:27:24 +0400

    added tab logo +title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title>Elite Residence</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/svgs/tab-logo.svg" />
+        </Head>
       <body>
         <Main />
         <NextScript />
