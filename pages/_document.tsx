bf8d4878ed1205b7814b8bb632deb4ee867dee52
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-06-11 12:48:27 +0400

    added metadata for SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
+      <Head>
         <title>Elite Residence</title>
-        <link rel="shortcut icon" type="image/x-icon" href="/svgs/tab-logo.svg" />
-        </Head>
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/svgs/tab-logo.svg"
+        />
+        <meta
+          name="description"
+          content="Low price flats, easy to purchase. Residence with modern design,"
+        />
+        <meta name="keywords" content="Residence, Flat, Sale Flats" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
